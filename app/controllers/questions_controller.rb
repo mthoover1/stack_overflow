fbf5a3f0155d20e75e05a107630382b117203bd9
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 11:33:50 -0500

    Created routes in controller for new, create, view to facilitate users creating questions; also created views for new, show, index

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params[:question])
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+end
