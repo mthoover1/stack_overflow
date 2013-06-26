7152c0b981c7b8ce055c81a668cd138d52c1f97c
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 13:23:57 -0500

    Question create action created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,16 @@
 class QuestionsController < ApplicationController
+  
   def new
     @question = Question.new
   end
+
+  def create
+    @question = Question.new(params[:question])
+    if question.save
+      render :index
+    else
+      render :new
+    end
+  end
+
 end
