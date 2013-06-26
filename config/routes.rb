b7475740b433c7fad0c2a8c9121aca6a894d48a4
Blurredbits <mmorris00@mac.com>
2013-06-26 17:09:54 -0500

    adjusted routes file for micro-challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 StackOverflow::Application.routes.draw do
-  resources :questions
-  resources :users
+  #resources :questions
+  #resources :users
+
+  get '/users' => 'users#index'
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+  get '/users/:id' => 'users#show'
+
+  get '/questions' => 'questions#index'
+  get '/questions/new' => 'questions#new'
+  get '/questions' => 'questions#create'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130626155738) do
+ActiveRecord::Schema.define(:version => 20130626203305) do
+
+  create_table "answers", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
 
   create_table "questions", :force => true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
@@ -27,6 +44,15 @@ ActiveRecord::Schema.define(:version => 20130626155738) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+  end
+
+  create_table "votes", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "value"
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
