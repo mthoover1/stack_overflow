27090a88e94c6ef83820bce2788fa726c8314f67
Daniel Song <danielx328@gmail.com>
2013-06-26 15:34:34 -0500

    modified question model to be commentable and votable

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,8 @@ class Question < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :comments, :as => :commentable
+  has_many :votes,    :as => :votable
+
   validates_presence_of :title, :content
 end
