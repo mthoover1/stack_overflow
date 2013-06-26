9c68eb34405306cdf33707658431e812b85a9da9
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 13:45:03 -0500

    made new user form with authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :questions
 
+  has_secure_password
+
+  attr_accessible :username, :email, :password
+
   validates :username, uniqueness: true
   validates :email,    uniqueness: true
   validates :email,    format: { :with => /[a-z0-9]+@[a-z0-9]{2,}\.[a-z]{2,}/i,
