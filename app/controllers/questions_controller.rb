bdb28f33a338546639155fd3da2a498b6ef76cbf
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-26 13:53:16 -0500

    Updated index action to show all questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,17 @@
 class QuestionsController < ApplicationController
-  
+
+  def index
+    @questions = Question.all
+  end
+
   def new
     @question = Question.new
   end
 
   def create
     @question = Question.new(params[:question])
-    if question.save
-      render :index
+    if @question.save
+      redirect_to questions_path
     else
       render :new
     end
