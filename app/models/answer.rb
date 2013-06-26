ca91bc69ddee8ff0c74a12a0f4e5c8295505fb96
Daniel Song <danielx328@gmail.com>
2013-06-26 15:33:06 -0500

    answer model and migration

---
+++ app/models/answer.rb
@@ -0,0 +1,11 @@
+class Answer < ActiveRecord::Base
+
+	belongs_to :user
+	belongs_to :question
+
+	has_many :comments, :as => :commentable
+	has_many :votes,    :as => :votable
+
+	validates_presence_of :content
+
+end
