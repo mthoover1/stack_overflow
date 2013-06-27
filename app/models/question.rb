3455a21793840494347aee0778dc8d204a9b699d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-27 09:07:14 -0500

    made user_id attr_accessible in question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,8 @@ class Question < ActiveRecord::Base
 
   has_many :comments, :as => :commentable
   has_many :votes,    :as => :votable
+  has_many :questions_tags
+  has_many :tags, :through => :questions_tags
 
   validates_presence_of :title, :content
 end
